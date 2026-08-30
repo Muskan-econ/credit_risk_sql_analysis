abd65144b61206ba54cdbbc0f77a92191cfa66b9
Muskan Mittal <muskanmittal2406@gmail.com>
2026-08-31 03:27:23 +0530

    Add exploratory analysis SQL queries
    Added SQL queries for exploratory analysis on borrowers, loan purposes, loan grades, and age groups.

--- sql/sql/sql/03_exploratory_analysis.sql
+++ sql/sql/sql/03_exploratory_analysis.sql
@@ -1,3 +1,4 @@
+--- Types of borrowers
 SELECT
     person_home_ownership,
     COUNT(*) AS borrowers,
@@ -9,3 +10,39 @@ SELECT
 FROM credit_risk
 GROUP BY person_home_ownership
 ORDER BY borrowers DESC;
+
+--- Loan Purpose
+SELECT
+    loan_intent,
+    COUNT(*) AS loans,
+    ROUND(AVG(loan_amnt), 2) AS avg_loan_amount,
+    ROUND(AVG(loan_int_rate), 2) AS avg_interest_rate
+FROM credit_risk
+GROUP BY loan_intent
+ORDER BY loans DESC;
+
+--- Loan Grade
+SELECT
+    loan_grade,
+    COUNT(*) AS loans,
+    ROUND(AVG(loan_int_rate), 2) AS avg_interest_rate,
+    ROUND(AVG(loan_amnt), 2) AS avg_loan_amount
+FROM credit_risk
+GROUP BY loan_grade
+ORDER BY loan_grade;
+
+--- Age Group 
+SELECT
+    CASE
+        WHEN person_age < 25 THEN '18-24'
+        WHEN person_age < 35 THEN '25-34'
+        WHEN person_age < 45 THEN '35-44'
+        WHEN person_age < 55 THEN '45-54'
+        ELSE '55+'
+    END AS age_group,
+    COUNT(*) AS borrowers,
+    ROUND(AVG(person_income), 2) AS avg_income,
+    ROUND(AVG(loan_amnt), 2) AS avg_loan
+FROM credit_risk
+GROUP BY age_group
+ORDER BY age_group;
